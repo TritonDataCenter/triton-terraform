0a5f6cac3c5d8fb1a61552142ddecb4cc42a85e3
Brian Hicks <brian@brianthicks.com>
2015-11-30 10:55:48 -0600

    makefile: add

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 )
 
+const Name = "triton-terraform"
+const Version = "0.0.1"
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: Provider,
